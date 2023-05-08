93f9b226d758e5ac2b70ef4048481b28894b6bf5
MohamedEmad2002 <119817795+MohamedEmad2002@users.noreply.github.com>
2023-05-08 19:35:03 +0300

    first commit

---
+++ lib/ui/screens/result.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ResultPage extends StatelessWidget {
+  const ResultPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Result'),elevation: 0.0,centerTitle: true,),
+    );
+  }
+}
